bc123a7c3b545a5ee0dd8f75509a77e4a7e85fdf
= <=>
2020-09-09 16:55:54 +0300

    Initial Commit

---
+++ NicknameGeneratorU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NicknameGeneratorU
+//
+//  Created by Batuhan Karababa on 9.09.2020.
+//  Copyright © 2020 Batuhan Karababa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
